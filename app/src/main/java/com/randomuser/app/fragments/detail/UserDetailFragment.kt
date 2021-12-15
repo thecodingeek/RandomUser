5abcb6de89a5d57a484976cb306c9c3ed4919409
Gurparsad Singh <thecodingeek@gmail.com>
2021-12-15 14:39:33 +0530

    Added blank User Detail Fragment

---
+++ app/src/main/java/com/randomuser/app/fragments/detail/UserDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.randomuser.app.fragments.detail
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.randomuser.app.R
+
+class UserDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = UserDetailFragment()
+    }
+
+    private lateinit var viewModel: UserDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.user_detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(UserDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/randomuser/app/fragments/detail/UserDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.randomuser.app.fragments.detail
+
+import androidx.lifecycle.ViewModel
+
+class UserDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
