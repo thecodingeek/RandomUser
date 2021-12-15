d02e09674c12143d06955ae01f38480e47fc0b16
Gurparsad Singh <thecodingeek@gmail.com>
2021-12-15 13:04:06 +0530

    Added subpackage for activities

--- app/src/main/java/com/randomuser/app/activities/MainActivity.kt
+++ app/src/main/java/com/randomuser/app/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.randomuser.app
+package com.randomuser.app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.randomuser.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
